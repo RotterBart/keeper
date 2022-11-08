373fcaf8a5ea97d1a9331925e9ed3ed64857ef7a
RotterBart <110440940+RotterBart@users.noreply.github.com>
2022-11-08 17:43:05 +0600

    update

--- keeper/settings.py
+++ keeper/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Sandbox'
+    
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
         },
     },
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 WSGI_APPLICATION = 'keeper.wsgi.application'
 
